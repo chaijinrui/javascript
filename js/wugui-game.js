6af2d24828dcfab3b1cd05072bb73af6d12f70c1
sns-chaijinrui <chai0304@163.com>
2023-09-05 19:16:30 +0800

    提交一部分代码

---
+++ js/wugui-game.js
@@ -0,0 +1,12 @@
+
+window.onload=function () {
+
+    // 绑定向上移动按钮
+    var but1 = document.getElementById("but1");
+    but1.onclick=function () {
+        var img1 = document.getElementById("img1");
+        // img1.margin={img1.margin.}
+        console.log("向上移动")
+    }
+
+}
